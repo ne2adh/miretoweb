c0025ad3c7d3a3be17d8d20f61805888f3e817bc
Giancarlo Delgadillo <giancarlodcwork@gmail.com>
2020-04-24 00:03:37 -0400

    Updates

---
+++ precache-manifest.39229bdfd06199aaa0d23f5a8551713f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d45e5d882a355860b8bbea5a7e71a3e",
+    "url": "/miretoweb/index.html"
+  },
+  {
+    "revision": "6b058e9d850904cb041b",
+    "url": "/miretoweb/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "81b3c6ba303a3ae9c65c",
+    "url": "/miretoweb/static/css/main.9cb17295.chunk.css"
+  },
+  {
+    "revision": "6b058e9d850904cb041b",
+    "url": "/miretoweb/static/js/2.623fdf06.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/miretoweb/static/js/2.623fdf06.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81b3c6ba303a3ae9c65c",
+    "url": "/miretoweb/static/js/main.18c713cf.chunk.js"
+  },
+  {
+    "revision": "147f5a2a00bcf0ba082b",
+    "url": "/miretoweb/static/js/runtime-main.2b464976.js"
+  }
+]);
